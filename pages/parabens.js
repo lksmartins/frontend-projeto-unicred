28bcf84854d79a0dd5dca7cea3727570a1014f21
Lucas Martins <lksmartins11@gmail.com>
2021-02-17 13:25:13 -0300

    Audio autoplay fixed

--- pages/parabens.js
+++ pages/parabens.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
 import Head from 'next/head'
-import ReactAudioPlayer  from 'react-h5-audio-player'
+import AudioPlayer  from 'react-h5-audio-player'
 
 import 'react-h5-audio-player/lib/styles.css'
 
+import Modal from '../components/Modal'
+
 export default function Parabens() {
+
+  const [modalShow, setModalShow] = useState(true)
+
+    useEffect(() => {
+
+        window.addEventListener('click', ()=>{
+
+            const audio = document.querySelector('.audio audio')
+
+            audio.play()
+
+        })
+
+    })
 
   return (
     <div className="container" id="parabens">
@@ -14,17 +30,33 @@ export default function Parabens() {
 
       <main className="main">
 
+        <Modal 
+            show={modalShow}
+            onHide={()=>setModalShow(false)}
+            title="Parabéns!"
+            >
+            <p>Vocês completaram a missão com sucesso!</p>
+        </Modal>
+
         <p className="description">
           Parabéns Esquadrão Unicred!
         </p>
 
         <div className="grid fill">
 
-            <div className="col">
+            <AudioPlayer 
+                className="audio" 
+                src="/queen.mp3"
+                autoPlay
+                loop
+                volume={0.5}
+            />
+
+            <div className="col left">
                 <img className="rounded" src="/imgs/trophy.gif" />
             </div>
 
-            <div className="col">
+            <div className="col right">
                 <p>
                 Vocês conseguiram conquistar o “Cooperado Mais Difícil do Mundo.”
                 </p>
@@ -40,13 +72,7 @@ export default function Parabens() {
                 <h3>www.esquadraounicred.com.br/recompensa</h3>
             </div>
 
-            <ReactAudioPlayer 
-                id="audio_player"
-                className="audio" 
-                src="/queen.mp3"
-                autoPlay
-                loop
-            />
+            
         </div>
 
       </main>
