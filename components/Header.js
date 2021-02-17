bea94c47ad430970670f13782276a38b5574538d
Lucas Martins <lksmartins11@gmail.com>
2021-02-17 13:34:54 -0300

    Link removed from Header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
 
 export default function Header() {
 
     return (
         <div className="header">
             <div className="content">
-                <Link href="/">
-                    <a><img src="/logo/logo_esquadrao_unicred.webp"/></a>
-                </Link>
+                <img src="/logo/logo_esquadrao_unicred.webp"/>
             </div>
         </div>
     )
