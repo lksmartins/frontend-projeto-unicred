a5074a32b69ff86d34e76e68cbace9855a207d39
Lucas Martins <lksmartins11@gmail.com>
2021-04-09 19:29:58 -0300

    pos-jogo content update

--- pages/v2/pos-jogo.js
+++ pages/v2/pos-jogo.js
@@ -29,7 +29,7 @@ export default function Parabens() {
     }, [])
 
     return (
-        <div className="container" id="parabens">
+        <div className="container" id="pos-jogo">
         <Head>
             <title>Projeto Unicred</title>
         </Head>
@@ -44,7 +44,9 @@ export default function Parabens() {
                 <p>Vocês completaram a missão com sucesso!</p>
             </Modal>
 
-            <h1 className="mt-60">
+            <img src="/logo/logo-escape-insta.svg" className="mt-60 rounded" alt="Chave-Mestra Logo" width="180" />
+
+            <h1 className="mt-30">
                 Parabéns!
             </h1>
 
@@ -58,39 +60,14 @@ export default function Parabens() {
                     volume={0.5}
                 />
 
-                <div className="col left">
+                <div className="col">
                     <img className="rounded" src="/imgs/trophy.gif" />
                 </div>
-
-                <div className="col right">
-                    <p>
-                    Vocês conseguiram conquistar o “Cooperado Mais Difícil do Mundo.”
-                    </p>
-                    <p>
-                    Através do jeito Eleva de ser, ao se IMPORTAR genuinamente em conquistar este cooperado, superando os desafios com ética, colaboração e aprendizado contínuo, vocês fizeram acontecer e provaram que juntos podem vencer e conquistar até mesmo o “Cooperado Mais Difícil do Mundo”.
-                    </p>
-                    <p>
-                    Ele ficou tão encantado com o trabalho de vocês, que irá lhes dar uma recompensa. Esta recompensa é individual e intransferível e para acessá-la você deve utilizar o cartão com sua senha pessoal. 
-                    </p>
-                    <p>
-                    Para acessar a sua recompensa individual acesse o link: 
-                    </p>
-                </div>
-
                 
             </div>
 
         </main>
-
-        <footer className="footer">
-            <a
-            href="https://chavemestra.net"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <img src="/logo/full_black.svg" alt="Chave-Mestra Logo" className="logo" />
-            </a>
-        </footer>
+        
         </div>
     )
 }
