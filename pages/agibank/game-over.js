7f47732eb3e91b32a612f7f11255789dbe48ab8e
Lucas Martins <lksmartins11@gmail.com>
2021-04-26 18:06:38 -0300

    agibank: game-over page added

---
+++ pages/agibank/game-over.js
@@ -0,0 +1,34 @@
+import React,{ useEffect, useState } from 'react'
+import Head from 'next/head'
+
+export default function GameOver() {
+
+    return (
+        <div className="container" id="agibank">
+        <Head>
+            <title>Agibank - Escape Live</title>
+        </Head>
+
+        <main className="main">
+
+            <img src="/logo/logo-escape-insta.svg" className="mt-60 rounded" alt="Chave-Mestra Logo" width="180" />
+
+            <h1 className="mt-30">
+                Não foi dessa vez!
+            </h1>
+
+            <div className="grid fill">
+
+                <div className="col mt-30 mb-30 text-center">
+                    <h3>
+                    Você não concluiu o jogo, mas isso não significa que não houve aprendizados. Essa experiência não é sobre escapar, mas sim sobre aprender algo novo. Reflita sobre essa experiência e logo mais converse com seus colegas.
+                    </h3>
+                </div>
+                
+            </div>
+
+        </main>
+        
+        </div>
+    )
+}
